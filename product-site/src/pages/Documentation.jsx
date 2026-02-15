4a2a2537771972522d7975729f2712d0b14e28bb
Rameez Malik <rmalik.ellahi@gmail.com>
2026-02-15 02:10:51 -0500

    Remove documentation heading from page.

--- product-site/src/pages/Documentation.jsx
+++ product-site/src/pages/Documentation.jsx
@@ -6,14 +6,6 @@ function Documentation() {
     <div className="page">
       <Header />
       <main className="page-content">
-        <motion.h1
-          className="page-title"
-          initial={{ opacity: 0, y: 30 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.7, delay: 0.2 }}
-        >
-          Documentation
-        </motion.h1>
       </main>
     </div>
   )
